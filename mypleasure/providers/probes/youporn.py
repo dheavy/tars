c9a823c25e758e0ffcf51fedb3863d21a4dc644f
Davy Peter Braun <hello@davybraun.com>
2014-12-28 02:38:01 +0100

    Fix duration formatting for Youporn.

--- mypleasure/providers/probes/youporn.py
+++ mypleasure/providers/probes/youporn.py
@@ -9,6 +9,7 @@ class Youporn(BaseProbe):
 
   name = "Youporn"
 
+
   def process(self):
     response = requests.get(self.url)
     markup = BeautifulSoup(response.text, 'lxml')
@@ -22,20 +23,28 @@ class Youporn(BaseProbe):
 
     markup.decompose()
 
+
   def __extract_id(self):
     return re.search("watch/(\d+)/", self.url).group(1)
+
 
   def __scrape_title(self, markup):
     return markup.select('#watchHeader h1')[0].string
 
+
   def __scrape_poster(self, markup):
     return markup.select('#player-html5')[0]['poster']
+
 
   def __scrape_duration(self):
     response = requests.get('http://www.youporn.com/search/?query=' + self.data['title'])
 
     markup = BeautifulSoup(response.text, 'lxml')
     duration = markup.select('.videoList .duration')[0].string
+
+    # Format hours display (two digits)
+    if len(duration) < 8:
+      duration = '0' + duration
 
     markup.decompose()
 
