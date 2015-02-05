bb6a61c8e93caec8ee1c4a5220d8ad86359b9876
Davy Peter Braun <hello@davybraun.com>
2015-02-05 17:51:22 +0000

    Build probe for Youtube provider.

---
+++ mypleasure/providers/probes/youtube.py
@@ -0,0 +1,45 @@
+import requests
+import urlparse
+import re
+from mypleasure.providers.probes.base import BaseProbe
+
+
+class Youtube(BaseProbe):
+  """A probe class to crawl and scrape Youtube videos."""
+
+  name = "Youtube"
+
+
+  def process(self):
+    response = requests.get(self.url)
+
+    self.id = self.__extract_id()
+    api_call_url = 'http://gdata.youtube.com/feeds/api/videos/' + self.id + '?v=2&alt=jsonc'
+    api_data = requests.get(api_call_url).json()['data']
+
+    self.data['title'] = self.__scrape_title(api_data)
+    self.data['poster'] = self.__scrape_poster()
+    self.data['method'] = 'iframe'
+    self.data['embed_url'] = 'https://www.youtube.com/embed/' + self.id
+    self.data['duration'] = self.__scrape_duration(api_data)
+
+
+  def __extract_id(self):
+    url_data = urlparse.urlparse(self.url)
+    query = urlparse.parse_qs(url_data.query)
+    return query['v'][0]
+
+
+  def __scrape_title(self, data):
+    return data['title']
+
+  def  __scrape_poster(self):
+    return 'http://img.youtube.com/vi/' + self.id + '/mqdefault.jpg'
+
+
+  def __scrape_duration(self, data):
+    duration = data['duration']
+    m, s = divmod(duration, 60)
+    h, m = divmod(m, 60)
+    duration = "%02d:%02d:%02d" % (h, m, s)
+    return duration
