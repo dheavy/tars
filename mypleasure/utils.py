762b0adc60222402b0b02ffcb4f4bbebd53ddf10
Davy Peter Braun <hello@davybraun.com>
2015-11-03 16:30:23 +0100

    Change default value of logger error method args and stringify data object.

--- mypleasure/utils.py
+++ mypleasure/utils.py
@@ -9,9 +9,9 @@ class Logger:
     def trace(self, msg):
         print(msg)
 
-    def error(self, url=None, msg=None, data=None):
+    def error(self, url='', msg='', data=None):
         report = '----\nERROR :(\n' + 'Job URL: ' + url + '\n' + msg
         if data:
-            report += data
+            report += str(data)
         print(report)
         # TODO: send email and Slack message
