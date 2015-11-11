a25eabb8c6e4f7d8721bd4f1c5ec62ffb97ca0b3
Davy Peter Braun <hello@davybraun.com>
2015-11-11 07:06:53 +0100

    Outline Logger's usage in docstring + basic verbosity/reporting level.

--- mypleasure/utils.py
+++ mypleasure/utils.py
@@ -5,13 +5,31 @@ from datetime import datetime
 
 class Logger:
 
+    def __init__(self, verbosity=1, reporting=1):
+        '''
+        Logger util for relevant data and errors.
+
+        Args:
+            verbosity: 0 silences logger in console, 1 prints output.
+            reporting: 0 silences reporting,
+                       1 logs into logfile,
+                       2 sends Slack message only,
+                       3 sends email only,
+                       4 sends both Slack and email messages.
+                       Both 2, 3 and 4 also print to logfile.
+        '''
+        self.verbosity = verbosity
+        self.reporting = reporting
+
     def trace(self, msg):
-        print(msg)
+        if self.verbosity > 0:
+            print(msg)
 
     def error(self, url='', msg='', data=None):
         report = '----\n+ ERROR ({0}) +\n'.format(datetime.now())
         report += 'Job URL: ' + url + '\n' + msg
         if data:
             report += str(data)
-        print(report)
+        if self.verbosity > 0:
+            print(report)
         # TODO: send email and Slack message
