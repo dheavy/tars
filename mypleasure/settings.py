803611e25878af3f0504795aac4d87e994848bbd
Davy Peter Braun <hello@davybraun.com>
2015-11-11 16:42:05 +0100

    Add a TODO.

--- mypleasure/settings.py
+++ mypleasure/settings.py
@@ -7,7 +7,7 @@ try:
     dotenv_path = join(dirname(__file__), '../.env')
     load_dotenv(dotenv_path)
 except:
-    # log: .env not found
+    # TODO log: .env not found
     pass
 
 DB_NAME = os.environ.get('DB_NAME', '')
